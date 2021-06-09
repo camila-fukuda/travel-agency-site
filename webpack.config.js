9b8b36bfd677f4f95be2e957dea2346e2949af48
Camila Fukuda <camilaffukuda@gmail.com>
2021-06-08 23:04:10 -0300

    Added postCss mixins configuration and experimented with media queries to change font size.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path")
 
-const postCSSPlugins = [require('postcss-import'),require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+const postCSSPlugins = [require('postcss-import'),
+require('postcss-mixins'),
+require("postcss-simple-vars"),
+require("postcss-nested"),
+require("autoprefixer")]
 
 module.exports = {
     // where to start bundling
